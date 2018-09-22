b8ec084921b019a4b7f065faf29ca35529ad7078
elisabetros <32452401+elisabetros@users.noreply.github.com>
2018-09-22 13:24:57 +0200

    satanic phonenumber input

---
+++ w4d4 - if satan was a web developer part 1/mysolution/main.js
@@ -0,0 +1,31 @@
+const plusBtn = document.querySelector("#plus");
+let firstCounter = 1;
+let secondCounter = 1;
+let firstSpan = document.querySelector("span:first-child");
+let secondSpan = document.querySelector("span:last-child");
+function counting() {
+  plusBtn.addEventListener("click", function() {
+    //   console.log(counter);
+    if (secondSpan.innerHTML <= 9) {
+      secondSpan.innerHTML = "000" + firstCounter++;
+    } else if (secondSpan.innerHTML <= 99) {
+      secondSpan.innerHTML = "00" + firstCounter++;
+    } else if (secondSpan.innerHTML <= 999) {
+      secondSpan.innerHTML = "0" + firstCounter++;
+    } else if (secondSpan.innerHTML >= 1000) {
+      secondSpan.innerHTML = firstCounter++;
+    }
+    if (secondSpan.innerHTML >= 9999) {
+      firstSpan.innerHTML = "00" + secondCounter++;
+      firstCounter = 0;
+      if (firstSpan.innerHTML >= 99) {
+        firstSpan.innerHTML = "0" + secondCounter++;
+      } else if (firstSpan.innerHTML >= 999) {
+        irstSpan.innerHTML = secondCounter++;
+      }
+      // secondSpan.innerHTML = "00" + firstcounter++;
+    }
+  });
+}
+
+counting();
