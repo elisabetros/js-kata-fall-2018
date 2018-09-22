c7643ab84f857d8029a61db5c3e8acfb2dc28175
elisabetros <32452401+elisabetros@users.noreply.github.com>
2018-09-22 13:48:51 +0200

    changed if statements

--- w4d4 - if satan was a web developer part 1/mysolution/main.js
+++ w4d4 - if satan was a web developer part 1/mysolution/main.js
@@ -1,33 +1,38 @@
 "use strict";
 const plusBtn = document.querySelector("#plus");
-let firstCounter = 1;
-let secondCounter = 1;
+let firstCounter = 0;
+let secondCounter = 0;
 let firstSpan = document.querySelector("span:first-child");
 let secondSpan = document.querySelector("span:last-child");
 
-function counting() {
-  plusBtn.addEventListener("click", function() {
-    //   console.log(counter);
-    if (secondSpan.innerHTML <= 9) {
-      secondSpan.innerHTML = "000" + firstCounter++;
-    } else if (secondSpan.innerHTML <= 99) {
-      secondSpan.innerHTML = "00" + firstCounter++;
-    } else if (secondSpan.innerHTML <= 999) {
-      secondSpan.innerHTML = "0" + firstCounter++;
-    } else if (secondSpan.innerHTML >= 1000) {
-      secondSpan.innerHTML = firstCounter++;
-    }
-    if (secondSpan.innerHTML >= 9999) {
-      firstSpan.innerHTML = "00" + secondCounter++;
-      firstCounter = 0;
-      if (firstSpan.innerHTML >= 99) {
-        firstSpan.innerHTML = "0" + secondCounter++;
-      } else if (firstSpan.innerHTML >= 999) {
-        firstSpan.innerHTML = secondCounter++;
-      }
-      // secondSpan.innerHTML = "00" + firstcounter++;
-    }
-  });
-}
+// setInterval(function() {
+function counting() {}
+plusBtn.addEventListener("click", function() {
+  secondSpan.innerHTML = firstCounter++;
+  //   console.log(counter);
+  if (firstCounter <= 9) {
+    secondSpan.innerHTML = "000" + firstCounter;
+  } else if (firstCounter <= 99) {
+    secondSpan.innerHTML = "00" + firstCounter;
+  } else if (firstCounter <= 999) {
+    secondSpan.innerHTML = "0" + firstCounter;
+  } else if (firstCounter >= 1000) {
+    secondSpan.innerHTML = firstCounter;
+  }
 
-counting();
+  if (firstCounter >= 9999) {
+    firstSpan.innerHTML = secondCounter++;
+    firstCounter = 0;
+  }
+  if (secondCounter <= 9) {
+    firstSpan.innerHTML = "00" + secondCounter;
+  } else if (secondCounter <= 99) {
+    firstSpan.innerHTML = "0" + secondCounter;
+  } else if (secondCounter <= 999) {
+    firstSpan.innerHTML = secondCounter;
+  }
+  // secondSpan.innerHTML = "00" + firstcounter++;
+});
+// }, 1);
+
+// counting();
